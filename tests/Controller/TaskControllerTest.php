5b4e5940f467aeed00b6c7c37fe08d1a202722e6
CorentinBorges <56678169+CorentinBorges@users.noreply.github.com>
2020-12-02 08:32:04 +0100

    tests bases ok

--- tests/Controller/TaskControllerTest.php
+++ tests/Controller/TaskControllerTest.php
@@ -6,11 +6,10 @@ namespace App\Tests\Controller;
 
 use App\Tests\AbstractWebTestCase;
 
-class TestControllerTest extends AbstractWebTestCase
+class TaskControllerTest extends AbstractWebTestCase
 {
     public function testListActionWithoutUser()
     {
         $this->client->request('GET', '/tasks');
-        self::assertStringContainsString();
     }
 }
