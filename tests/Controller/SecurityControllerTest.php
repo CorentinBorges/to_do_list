44251a6d3160a35033f1e88b5416785b526441c8
CorentinBorges <56678169+CorentinBorges@users.noreply.github.com>
2020-12-03 15:05:32 +0100

    SecurityController tests ok

--- tests/Controller/SecurityControllerTest.php
+++ tests/Controller/SecurityControllerTest.php
@@ -8,6 +8,37 @@ use App\Tests\AbstractWebTestCase;
 
 class SecurityControllerTest extends AbstractWebTestCase
 {
+    public function testLoginActionPageWithWrongUser()
+    {
+        $uriLogin = $this->client->request('GET', '/login')->getBaseHref();
+
+        $crawler = $this->client->request('GET', '/login');
+        $form = $crawler->selectButton('Se connecter')->form();
+
+        $form['_username']='userTest';
+        $form['_password']='testPass';
+
+        $this->client->submit($form);
+
+        self::assertEquals(302,$this->client->getResponse()->getStatusCode());
+        self::assertResponseRedirects($uriLogin);
+    }
+
+    public function testLoginActionPageWithGoodData()
+    {
+        $uriHome = $this->client->request('GET', '/')->getBaseHref();
+
+        $this->createUser();
+        $crawler = $this->client->request('GET', '/login');
+        $form = $crawler->selectButton('Se connecter')->form();
+
+        $form['_username']='userTest';
+        $form['_password']='testPass';
+
+        $this->client->submit($form);
+        self::assertEquals(302,$this->client->getResponse()->getStatusCode());
+        self::assertResponseRedirects($uriHome);
+    }
 
     public function testLoginActionPage()
     {
